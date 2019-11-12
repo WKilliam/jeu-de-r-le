2c1b26a0db8a537dbc16c460376e17176c8165a6
teddy <william.k97430@gmail.com>
2019-11-12 15:25:24 +0100

    teddytest

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int test(){
+        
+    }
+
 
     public static void main(String[] args) {
 	// write your code here
-
-        Scanner keyBoard_Origin = new Scanner(System.in);
-
-        String key_Input = keyBoard_Origin.next();
+        
 
     }
 }
