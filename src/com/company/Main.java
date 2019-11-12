4967c3561773642ecec5a82a2e2f9e7b26235cbf
teddy <william.k97430@gmail.com>
2019-11-12 10:03:53 +0100

    fonction_input_simple_complet_ASCII

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,7 +18,190 @@ public class Main {
 
         for (i = 0; i < key_Input_INT; i = i + 1) {
             System.out.println(key_Input.charAt(i));
+            if (key_test == 48) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 49) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 50) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 51) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 52) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 53) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 54) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 55) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 56) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 57) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 65) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 66) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 67) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 68) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 69) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 70) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 71) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 72) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 73) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 74) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 75) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 76) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 77) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 78) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 79) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 80) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 81) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 82) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 83) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 84) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 85) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 86) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 87) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 88) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 89) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 90) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 97) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 98) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 99) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 100) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 101) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 102) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 103) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 104) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 105) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 106) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 107) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
             if (key_test == 108) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 109) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 110) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 111) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 112) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 113) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 114) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 115) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 116) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 117) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 118) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 119) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 120) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 121) {
+                System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
+            }
+            if (key_test == 122) {
                 System.out.println("j'ai trouvé un :" + key_Input.charAt(i));
             }
         }
